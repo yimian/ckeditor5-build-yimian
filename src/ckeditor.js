7d30e02c7751f9b4ef6cc0cd1b19db57929f266e
sishenhei7 <982505136@qq.com>
2020-02-28 11:23:55 +0800

    fix: 修改center-default的icon

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -31,7 +31,8 @@ import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 import fullSizeIcon from '@ckeditor/ckeditor5-core/theme/icons/object-center.svg';
 import sideIcon from '@ckeditor/ckeditor5-core/theme/icons/object-right.svg';
-import defaultCenterIcon from './center-default.svg';
+import defaultCenterIcon from '@ckeditor/ckeditor5-core/theme/icons/object-left.svg';
+// import defaultCenterIcon from './center-default.svg';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
